3b921f429d38a3fed2559eecc262ca3de3650bf3
joshuamURD <jmacmaster9@gmail.com>
2024-05-28 12:01:11 +0800

    Adding SVG and changing 404 page

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,15 +7,38 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h1 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h1>
+    <div class="page-title-container">
+      <img
+        src="https://assets-global.website-files.com/5e51c674258ffe10d286d30a/5e5354e49588e03c817d03a4_peep-33.svg"
+        alt=""
+      />
+      <h1 class={classNames(displayClass, "page-title")}>
+        <a href={baseDir}>{title}</a>
+      </h1>
+    </div>
   )
 }
 
 PageTitle.css = `
 .page-title {
   margin: 0;
+}
+@keyframes hideshow {
+  from {
+    opacity: 0;
+    width:0%;
+  }
+  to {
+    opacity: 1;
+    width:30%:
+  }
+} 
+div.page-title-container img {
+  animation: hideshow 1.5s ease;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width:30%;
 }
 `
 
--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -4,7 +4,8 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   return (
     <article class="popover-hint">
-      <h1>Do not wonder in the woods...</h1>
+      <h2>Do not wonder in the woods...</h2>
+      <img src="static/undraw_cat_epte.svg" alt="My Happy SVG" />
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
     </article>
   )
